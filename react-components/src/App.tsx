e5de8a16b569396044be76317dacac70dc4e2086
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-03-13 12:50:00 +0200

    feat: clean initialized project

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,22 +1,7 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  
-
-  return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+  return <div className="App">Let&apos;s get this party started</div>;
 }
 
 export default App;
