5eae17fa49bc0874e6a1de37a6466c66f064ea5f
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-03-16 20:07:47 +0200

    feat: add header component

---
+++ react-components/src/components/header/header.tsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import { NavLink, NavLinkProps } from 'react-router-dom';
+import styles from './header.module.scss';
+import cn from 'classnames';
+import { Container } from '../ui/container/container';
+
+export class Header extends Component {
+  static readonly classNames = {
+    link: styles.nav__link,
+    linkActive: styles.nav__link_active,
+  };
+
+  className: NavLinkProps['className'] = ({ isActive }) =>
+    isActive ? cn(Header.classNames.link, Header.classNames.linkActive) : Header.classNames.link;
+
+  render() {
+    return (
+      <header className={styles.header}>
+        <Container>
+          <nav>
+            <NavLink to="/" className={this.className}>
+              Home
+            </NavLink>
+            <NavLink to="/about" className={this.className}>
+              About Us
+            </NavLink>
+          </nav>
+        </Container>
+      </header>
+    );
+  }
+}
--- react-components/src/components/layout/layout.tsx
+++ react-components/src/components/layout/layout.tsx
@@ -1,33 +1,12 @@
 import { Component } from 'react';
-import { NavLink, NavLinkProps, Outlet } from 'react-router-dom';
-import styles from './layout.module.scss';
-import cn from 'classnames';
-import { Container } from '../ui/container/container';
+import { Outlet } from 'react-router-dom';
+import { Header } from '../header/header';
 
 export class Layout extends Component {
-  static readonly classNames = {
-    link: styles.nav__link,
-    linkActive: styles.nav__link_active,
-  };
-
-  className: NavLinkProps['className'] = ({ isActive }) =>
-    isActive ? cn(Layout.classNames.link, Layout.classNames.linkActive) : Layout.classNames.link;
-
   render() {
     return (
       <>
-        <header className={styles.header}>
-          <Container>
-            <nav>
-              <NavLink to="/" className={this.className}>
-                Home
-              </NavLink>
-              <NavLink to="/about" className={this.className}>
-                About Us
-              </NavLink>
-            </nav>
-          </Container>
-        </header>
+        <Header />
         <main className="main">
           <Outlet />
         </main>
