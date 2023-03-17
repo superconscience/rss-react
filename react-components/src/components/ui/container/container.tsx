69794a85f269e7dd8eef45af6213eda56cf1c5dc
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-03-17 09:12:13 +0200

    feat: add tests for Container component

--- react-components/src/components/ui/container/container.tsx
+++ react-components/src/components/ui/container/container.tsx
@@ -6,14 +6,18 @@ import styles from './container.module.scss';
 export type ContainerProps = PropsWithClassNameAndChildren & { bp?: Breakpoint };
 
 export class Container extends Component<ContainerProps> {
-  render() {
+  getBreakpointClassName = (): string => {
     const { bp } = this.props;
     let classNameKey = 'container';
     if (bp) {
       classNameKey += `-${bp}`;
     }
+    return styles[classNameKey];
+  };
+
+  render() {
     return (
-      <div className={cn(this.props.className, styles[classNameKey])}>
+      <div className={cn(this.props.className, this.getBreakpointClassName())}>
         {this.props.children && this.props.children}
       </div>
     );
