4c970e87d22f67bd47fef9c56c97f2b5c8ded195
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-03-30 17:05:45 +0300

    refactor: refactor Layout component to FC

--- react-components/src/components/layout/layout.tsx
+++ react-components/src/components/layout/layout.tsx
@@ -1,16 +1,12 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../header/header';
 
-export class Layout extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <main className="main" data-testid="main">
-          <Outlet />
-        </main>
-      </>
-    );
-  }
-}
+export const Layout: FC = () => (
+  <>
+    <Header />
+    <main className="main" data-testid="main">
+      <Outlet />
+    </main>
+  </>
+);
