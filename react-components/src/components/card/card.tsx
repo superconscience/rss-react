9090684093f1f1b07508199dbbfc13c1739d5235
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-03-30 16:46:21 +0300

    refactor: switch to hooks in Card component

--- react-components/src/components/card/card.tsx
+++ react-components/src/components/card/card.tsx
@@ -1,12 +1,12 @@
-import { Component } from 'react';
+import cn from 'classnames';
+import { FC, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Card as CardType } from '../../data/cards';
 import { PropsWithClassName } from '../../types/types';
-import cn from 'classnames';
-import styles from './card.module.scss';
-import { Link } from 'react-router-dom';
 import { humanizeNumber } from '../../utils/functions';
 import { LikeIcon } from '../ui/like-icon';
 import { ViewsIcon } from '../ui/views-icon';
+import styles from './card.module.scss';
 
 export type CardProps = PropsWithClassName & { card: CardType };
 
@@ -14,57 +14,53 @@ export type CardState = {
   image: string | undefined;
 };
 
-export class Card extends Component<CardProps, CardState> {
-  state: CardState = {
-    image: undefined,
-  };
+export const Card: FC<CardProps> = ({ card, className }) => {
+  const { title, author, tags, link, likes, views, image: cardImage } = card;
 
-  componentDidMount(): void {
-    this.loadImage(this.props.card.image);
-  }
+  const [image, setImage] = useState<CardState['image']>();
 
-  loadImage = async (filename: string) => {
+  const loadImage = async (filename: string) => {
     const sep = '.';
     const [ext, ...nameParts] = filename.split(sep).reverse();
     const name = nameParts.join(sep);
     const image = await import(`../../assets/images/${name}.${ext}`);
-    this.setState({
-      image: image.default,
-    });
+    setImage(image.default);
   };
 
-  render() {
-    const { title, author, tags, link, likes, views } = this.props.card;
-    const { image } = this.state;
-    return (
-      <li className={cn(this.props.className, styles.card)}>
-        <img className={styles.card__image} src={image} alt={title} />
-        <div className={styles.card__middle}>
-          <div className={styles.card__description}>
-            <Link to="/" className={styles.card__title}>
-              {title}
+  useEffect(() => {
+    if (cardImage) {
+      loadImage(cardImage);
+    }
+  }, [cardImage]);
+
+  return (
+    <li className={cn(className, styles.card)}>
+      <img className={styles.card__image} src={image} alt={title} />
+      <div className={styles.card__middle}>
+        <div className={styles.card__description}>
+          <Link to="/" className={styles.card__title}>
+            {title}
+          </Link>
+          <div className={styles.card__author}>
+            by&nbsp;
+            <Link to={link} className={styles['card__author-link']}>
+              {author}
             </Link>
-            <div className={styles.card__author}>
-              by&nbsp;
-              <Link to={link} className={styles['card__author-link']}>
-                {author}
-              </Link>
-            </div>
-          </div>
-          <hr className={styles.card__separator} />
-          <div>
-            <span className={styles.card__tags}>{tags}</span>
           </div>
         </div>
-        <div className={styles.card__bottom}>
-          <div>
-            <LikeIcon fill="#989898" /> {humanizeNumber(likes)}
-          </div>
-          <div>
-            <ViewsIcon fill="#989898" /> {humanizeNumber(views)}
-          </div>
+        <hr className={styles.card__separator} />
+        <div>
+          <span className={styles.card__tags}>{tags}</span>
         </div>
-      </li>
-    );
-  }
-}
+      </div>
+      <div className={styles.card__bottom}>
+        <div>
+          <LikeIcon fill="#989898" /> {humanizeNumber(likes)}
+        </div>
+        <div>
+          <ViewsIcon fill="#989898" /> {humanizeNumber(views)}
+        </div>
+      </div>
+    </li>
+  );
+};
