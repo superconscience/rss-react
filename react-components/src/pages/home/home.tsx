45a9670c3d03f9ff69c1bf13198f3652a4e2a3fb
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-04-01 15:11:40 +0300

    refactor: refactor Home page component to FC

--- react-components/src/pages/home/home.tsx
+++ react-components/src/pages/home/home.tsx
@@ -1,26 +1,22 @@
-import { Grid } from '../../components/grid/grid';
-import { Component } from 'react';
-import cards from '../../data/cards';
+import { FC } from 'react';
 import { Card } from '../../components/card/card';
+import { Grid } from '../../components/grid/grid';
 import { Search } from '../../components/search/search';
 import { Container } from '../../components/ui/container/container';
+import cards from '../../data/cards';
 import styles from './home.module.scss';
 
-export class Home extends Component {
-  render() {
-    return (
-      <Container>
-        <div className={styles['search-wrapper']}>
-          <Search />
-        </div>
-        <div className={styles['cards-wrapper']}>
-          <Grid>
-            {cards.map((card) => (
-              <Card key={card.title} card={card} />
-            ))}
-          </Grid>
-        </div>
-      </Container>
-    );
-  }
-}
+export const Home: FC = () => (
+  <Container>
+    <div className={styles['search-wrapper']}>
+      <Search />
+    </div>
+    <div className={styles['cards-wrapper']}>
+      <Grid>
+        {cards.map((card) => (
+          <Card key={card.title} card={card} />
+        ))}
+      </Grid>
+    </div>
+  </Container>
+);
