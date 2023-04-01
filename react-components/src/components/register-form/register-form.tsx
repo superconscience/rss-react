96e430d49bb3402e634487e5bf8f45b4a59e8f52
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-04-01 15:52:49 +0300

    refactor: refactor RegisterForm component to FC

--- react-components/src/components/register-form/register-form.tsx
+++ react-components/src/components/register-form/register-form.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import { Component, FormEventHandler, RefObject, createRef } from 'react';
+import { FC, FormEventHandler, ReactNode, RefObject, createRef, useState } from 'react';
 import { User } from '../../models/user';
 import { getRandomId } from '../../utils/functions';
 import { CheckboxInput } from '../ui/checkbox/checkbox';
@@ -55,12 +55,25 @@ const registerFormInitialState: RegisterFormComponentState = {
   },
 };
 
-export class RegisterForm extends Component<RegisterFormProps, RegisterFormComponentState> {
-  static readonly classNames = {
-    wasValidated: 'was-validated',
-  };
+export const RegisterForm: FC<RegisterFormProps> = ({ addUser, showAlert }) => {
+  const [validated, setValidated] = useState<boolean>(false);
+  const [image, setImage] = useState<string | null>();
+  const [errors, setErrors] = useState<FormErrors>({
+    name: [],
+    lastName: [],
+    email: [],
+    birthdate: [],
+    state: [],
+    city: [],
+    image: [],
+    zip: [],
+    gender: [],
+    agree: [],
+  });
 
-  formElementsRefs: {
+  const formRef: RefObject<HTMLFormElement> = createRef<HTMLFormElement>();
+
+  const formElementsRefs: {
     [K in Exclude<KeysForRefs | keyof RegisterFormElements, 'gender'>]: K extends
       | 'genderMale'
       | 'genderFemale'
@@ -82,42 +95,27 @@ export class RegisterForm extends Component<RegisterFormProps, RegisterFormCompo
     agree: createRef<HTMLInputElement>(),
   };
 
-  formRef: RefObject<HTMLFormElement>;
-  state: RegisterFormComponentState = registerFormInitialState;
-
-  constructor(props: RegisterFormProps) {
-    super(props);
-    this.formRef = createRef<HTMLFormElement>();
-  }
-
-  useId = getRandomId();
-
-  handleSubmit: FormEventHandler<HTMLFormElement> = (event): void => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event): void => {
     event.preventDefault();
     const form = event.target;
     if (!(form instanceof HTMLFormElement)) {
       return;
     }
-    const errors = this.validate();
-    const isValid = !this.hasErrors(errors);
+    const errors = validate();
+    const isValid = !hasErrors(errors);
 
-    this.setState((prev) => {
-      return { ...prev, errors, validated: true };
-    });
+    setErrors(errors);
+    setValidated(true);
 
     if (isValid) {
-      const newUser = this.extractUser();
-      this.props.addUser(newUser);
-      this.props.showAlert();
-      this.resetForm();
+      const newUser = extractUser();
+      addUser(newUser);
+      showAlert();
+      resetForm();
     }
   };
 
-  setImage = (src: string | null) => {
-    this.setState({ image: src });
-  };
-
-  validate = (): FormErrors => {
+  function validate(): FormErrors {
     const errors = { ...registerFormInitialState.errors };
     const setError = (name: keyof typeof errors, result: ValidationResult) => {
       if (!result.isValid) {
@@ -129,13 +127,13 @@ export class RegisterForm extends Component<RegisterFormProps, RegisterFormCompo
       if (typedName === 'agree' || typedName === 'gender' || typedName === 'image') {
         return;
       }
-      const element = this.formElementsRefs[typedName].current;
+      const element = formElementsRefs[typedName].current;
       if (element) {
         const result = validate(element.value);
         setError(typedName, result);
       }
     });
-    const { image, agree, genderMale, genderFemale } = this.formElementsRefs;
+    const { image, agree, genderMale, genderFemale } = formElementsRefs;
     if (image.current) {
       const result = validation.image.validate(image.current.files?.[0] || null);
       setError('image', result);
@@ -151,23 +149,25 @@ export class RegisterForm extends Component<RegisterFormProps, RegisterFormCompo
       setError('gender', result);
     }
     return errors;
-  };
+  }
 
-  resetForm = () => {
-    this.formRef.current?.reset();
-    this.setImage(null);
-    this.setState({
-      errors: { ...registerFormInitialState.errors },
-      validated: false,
-    });
-  };
+  function resetForm(): void {
+    formRef.current?.reset();
+    setImage(null);
+    setErrors({ ...registerFormInitialState.errors });
+    setValidated(false);
+  }
 
-  hasErrors = (errors: FormErrors): boolean =>
-    Object.values(errors).some((messages) => messages.length > 0);
+  function hasErrors(errors: FormErrors): boolean {
+    return Object.values(errors).some((messages) => messages.length > 0);
+  }
 
-  extractUser = (): User => {
-    const { name, lastName, email, birthdate, state, city, zip, genderMale } =
-      this.formElementsRefs;
+  function setUploadImage(image: string | null): void {
+    setImage(image);
+  }
+
+  function extractUser(): User {
+    const { name, lastName, email, birthdate, state, city, zip, genderMale } = formElementsRefs;
     return {
       id: getRandomId(),
       name: name.current?.value || '',
@@ -177,13 +177,12 @@ export class RegisterForm extends Component<RegisterFormProps, RegisterFormCompo
       state: state.current?.value || '',
       city: city.current?.value || '',
       zip: Number(zip.current?.value) || 0,
-      image: this.state.image || '',
+      image: image || '',
       gender: genderMale.current?.checked ? 'male' : 'female',
     };
-  };
+  }
 
-  errorElements = (name: keyof RegisterFormElements) => {
-    const { errors } = this.state;
+  function errorElements(name: keyof RegisterFormElements): ReactNode {
     const messages = errors[name];
     return (
       (messages.length > 0 &&
@@ -194,161 +193,157 @@ export class RegisterForm extends Component<RegisterFormProps, RegisterFormCompo
         ))) ||
       null
     );
-  };
+  }
 
-  getValidationResultClassName = (
+  function getValidationResultClassName(
     name: keyof RegisterFormComponentState['errors']
-  ): ValidClassName | null => {
-    const { errors, validated } = this.state;
+  ): ValidClassName | null {
     if (!validated) {
       return null;
     }
     return errors[name].length > 0 ? 'invalid' : 'valid';
-  };
+  }
 
-  render() {
-    const { validated, image } = this.state;
-    return (
-      <>
-        <div className="row align-items-start border-end">
-          <div>
-            <h2>Add User</h2>
+  return (
+    <>
+      <div className="row align-items-start border-end">
+        <div>
+          <h2>Add User</h2>
+        </div>
+
+        <form
+          className={cn('row g-3', { 'was-validated': validated })}
+          ref={formRef}
+          onSubmit={handleSubmit}
+          noValidate={true}
+        >
+          <div className="col-md-12 col-sm-6 col-6">
+            <TextInput
+              label="Name"
+              name="name"
+              ref={formElementsRefs.name}
+              validClassName={getValidationResultClassName('name')}
+            />
+            {errorElements('name')}
+          </div>
+          <div className="col-md-12 col-sm-6 col-6">
+            <TextInput
+              label="Last Name"
+              name="lastName"
+              ref={formElementsRefs.lastName}
+              validClassName={getValidationResultClassName('lastName')}
+            />
+            {errorElements('lastName')}
+          </div>
+          <div className="col-md-12 col-sm-6 col-6">
+            <TextInput
+              label="Email"
+              name="email"
+              ref={formElementsRefs.email}
+              inputProps={{ type: 'email' }}
+              validClassName={getValidationResultClassName('email')}
+            />
+            {errorElements('email')}
+          </div>
+          <div className="col-md-12 col-sm-6 col-6">
+            <TextInput
+              label="Birth Date"
+              name="birthdate"
+              ref={formElementsRefs.birthdate}
+              inputProps={{ type: 'date' }}
+              validClassName={getValidationResultClassName('birthdate')}
+            />
+            {errorElements('birthdate')}
+          </div>
+          <div className="col-md-12 col-6">
+            <TextInput
+              label="City"
+              name="city"
+              ref={formElementsRefs.city}
+              validClassName={getValidationResultClassName('city')}
+            />
+            {errorElements('city')}
+          </div>
+          <div className="col-md-12 col-6">
+            <TextInput
+              label="Zip"
+              name="zip"
+              ref={formElementsRefs.zip}
+              inputProps={{ maxLength: 5 }}
+              validClassName={getValidationResultClassName('zip')}
+            />
+            {errorElements('zip')}
+          </div>
+          <div className="col-12">
+            <Select
+              label="State"
+              name="state"
+              ref={formElementsRefs.state}
+              validClassName={getValidationResultClassName('state')}
+            >
+              <option value={0} style={{ display: 'none' }}>
+                Choose state...
+              </option>
+              <option>Japan</option>
+              <option>USA</option>
+              <option>Italy</option>
+              <option>Latvia</option>
+            </Select>
+            {errorElements('state')}
           </div>
 
-          <form
-            className={cn('row g-3', { 'was-validated': validated })}
-            ref={this.formRef}
-            onSubmit={this.handleSubmit}
-            noValidate={true}
-          >
-            <div className="col-md-12 col-sm-6 col-6">
-              <TextInput
-                label="Name"
-                name="name"
-                ref={this.formElementsRefs.name}
-                validClassName={this.getValidationResultClassName('name')}
+          <div className="col-12">
+            <UploadImage
+              name="image"
+              setImage={setUploadImage}
+              ref={formElementsRefs.image}
+              validClassName={getValidationResultClassName('image')}
+            />
+            {errorElements('image')}
+            {image && <img src={image} alt={'image'} width="100%" className="mt-3" />}
+          </div>
+          <div className="col-12">
+            <label className="form-label">Gender</label>
+            <div>
+              <RadioInput
+                label="Male"
+                name="gender"
+                value="male"
+                ref={formElementsRefs.genderMale}
+                labelProps={{ className: 'btn' }}
+                inputProps={{ className: 'btn-check' }}
+                validClassName={getValidationResultClassName('gender')}
+                checked
               />
-              {this.errorElements('name')}
-            </div>
-            <div className="col-md-12 col-sm-6 col-6">
-              <TextInput
-                label="Last Name"
-                name="lastName"
-                ref={this.formElementsRefs.lastName}
-                validClassName={this.getValidationResultClassName('lastName')}
+              <RadioInput
+                label="Female"
+                name="gender"
+                value="female"
+                ref={formElementsRefs.genderFemale}
+                labelProps={{ className: 'btn' }}
+                inputProps={{ className: 'btn-check' }}
+                validClassName={getValidationResultClassName('gender')}
               />
-              {this.errorElements('lastName')}
+              {errorElements('gender')}
             </div>
-            <div className="col-md-12 col-sm-6 col-6">
-              <TextInput
-                label="Email"
-                name="email"
-                ref={this.formElementsRefs.email}
-                inputProps={{ type: 'email' }}
-                validClassName={this.getValidationResultClassName('email')}
+          </div>
+          <div className="col-12">
+            <div className="form-check">
+              <CheckboxInput
+                label="I agree with the fact that my data will be displayed somewhere on this page"
+                name="agree"
+                ref={formElementsRefs.agree}
+                validClassName={getValidationResultClassName('agree')}
               />
-              {this.errorElements('email')}
             </div>
-            <div className="col-md-12 col-sm-6 col-6">
-              <TextInput
-                label="Birth Date"
-                name="birthdate"
-                ref={this.formElementsRefs.birthdate}
-                inputProps={{ type: 'date' }}
-                validClassName={this.getValidationResultClassName('birthdate')}
-              />
-              {this.errorElements('birthdate')}
-            </div>
-            <div className="col-md-12 col-6">
-              <TextInput
-                label="City"
-                name="city"
-                ref={this.formElementsRefs.city}
-                validClassName={this.getValidationResultClassName('city')}
-              />
-              {this.errorElements('city')}
-            </div>
-            <div className="col-md-12 col-6">
-              <TextInput
-                label="Zip"
-                name="zip"
-                ref={this.formElementsRefs.zip}
-                inputProps={{ maxLength: 5 }}
-                validClassName={this.getValidationResultClassName('zip')}
-              />
-              {this.errorElements('zip')}
-            </div>
-            <div className="col-12">
-              <Select
-                label="State"
-                name="state"
-                ref={this.formElementsRefs.state}
-                validClassName={this.getValidationResultClassName('state')}
-              >
-                <option value={0} style={{ display: 'none' }}>
-                  Choose state...
-                </option>
-                <option>Japan</option>
-                <option>USA</option>
-                <option>Italy</option>
-                <option>Latvia</option>
-              </Select>
-              {this.errorElements('state')}
-            </div>
-
-            <div className="col-12">
-              <UploadImage
-                name="image"
-                setImage={this.setImage}
-                ref={this.formElementsRefs.image}
-                validClassName={this.getValidationResultClassName('image')}
-              />
-              {this.errorElements('image')}
-              {image && <img src={image} alt={'image'} width="100%" className="mt-3" />}
-            </div>
-            <div className="col-12">
-              <label className="form-label">Gender</label>
-              <div>
-                <RadioInput
-                  label="Male"
-                  name="gender"
-                  value="male"
-                  ref={this.formElementsRefs.genderMale}
-                  labelProps={{ className: 'btn' }}
-                  inputProps={{ className: 'btn-check' }}
-                  validClassName={this.getValidationResultClassName('gender')}
-                  checked
-                />
-                <RadioInput
-                  label="Female"
-                  name="gender"
-                  value="female"
-                  ref={this.formElementsRefs.genderFemale}
-                  labelProps={{ className: 'btn' }}
-                  inputProps={{ className: 'btn-check' }}
-                  validClassName={this.getValidationResultClassName('gender')}
-                />
-                {this.errorElements('gender')}
-              </div>
-            </div>
-            <div className="col-12">
-              <div className="form-check">
-                <CheckboxInput
-                  label="I agree with the fact that my data will be displayed somewhere on this page"
-                  name="agree"
-                  ref={this.formElementsRefs.agree}
-                  validClassName={this.getValidationResultClassName('agree')}
-                />
-              </div>
-            </div>
-            <div className="col-12">
-              <button type="submit" className="btn btn-primary">
-                Register
-              </button>
-            </div>
-          </form>
-        </div>
-      </>
-    );
-  }
-}
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
