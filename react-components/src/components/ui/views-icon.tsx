b1fe799a82a249e756094d8289f671ee43e1f44c
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-04-01 14:45:38 +0300

    refactor: refactor icon components to FC

--- react-components/src/components/ui/views-icon.tsx
+++ react-components/src/components/ui/views-icon.tsx
@@ -1,27 +1,20 @@
-import { Component } from 'react';
+import { FC } from 'react';
 
 export type ViewsIconProps = { width: number; height: number; fill: string };
 
-export class ViewsIcon extends Component<ViewsIconProps> {
-  static defaultProps: ViewsIconProps = {
-    width: 16,
-    height: 16,
-    fill: 'none',
-  };
+export const ViewsIcon: FC<ViewsIconProps> = ({ width, height, fill }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 16 16">
+      <path
+        fill={fill}
+        d="M8.5 3.5c-5 0-8 5-8 5s3 5 8 5 8-5 8-5-3-5-8-5zm0 7c-1.105 0-2-.896-2-2 0-1.106.895-2 2-2 1.104 0 2 .894 2 2 0 1.104-.896 2-2 2z"
+      ></path>
+    </svg>
+  );
+};
 
-  render() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={this.props.width}
-        height={this.props.height}
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill={this.props.fill}
-          d="M8.5 3.5c-5 0-8 5-8 5s3 5 8 5 8-5 8-5-3-5-8-5zm0 7c-1.105 0-2-.896-2-2 0-1.106.895-2 2-2 1.104 0 2 .894 2 2 0 1.104-.896 2-2 2z"
-        ></path>
-      </svg>
-    );
-  }
-}
+ViewsIcon.defaultProps = {
+  width: 16,
+  height: 16,
+  fill: 'none',
+};
