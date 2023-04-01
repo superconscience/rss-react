892d8562d4f8040b44b1aa1a93076c1b3f8ea11e
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-04-01 15:17:03 +0300

    refactor: refactor App component to FC

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,20 +1,16 @@
+import { FC } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import routes from './routes';
-import { Component } from 'react';
 import { AppContextProvider } from './context/app-context';
+import routes from './routes';
 
 const router = createBrowserRouter(routes);
 
-class App extends Component {
-  render() {
-    return (
-      <AppContextProvider>
-        <div className="App" data-testid="app">
-          <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
-        </div>
-      </AppContextProvider>
-    );
-  }
-}
+const App: FC = () => (
+  <AppContextProvider>
+    <div className="App" data-testid="app">
+      <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+    </div>
+  </AppContextProvider>
+);
 
 export default App;
