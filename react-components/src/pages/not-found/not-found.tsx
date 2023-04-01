18f0cb593c37e98974681a2223b1473a12e5509f
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-04-01 15:12:48 +0300

    refactor: refactor NotFound page component to FC

--- react-components/src/pages/not-found/not-found.tsx
+++ react-components/src/pages/not-found/not-found.tsx
@@ -1,19 +1,15 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { Header } from '../../components/header/header';
 import { Container } from '../../components/ui/container/container';
 import styles from './not-found.module.scss';
 
-export class NotFound extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <div className={styles['not-found']} data-testid="not-found">
-          <Container>
-            <p>Page Not Found 😩</p>
-          </Container>
-        </div>
-      </>
-    );
-  }
-}
+export const NotFound: FC = () => (
+  <>
+    <Header />
+    <div className={styles['not-found']} data-testid="not-found">
+      <Container>
+        <p>Page Not Found 😩</p>
+      </Container>
+    </div>
+  </>
+);
