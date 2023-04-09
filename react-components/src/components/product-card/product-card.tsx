9cda690ca6191d036c2db0e323f7c1f8dd847581
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-04-09 22:11:24 +0300

    feat: reuse useImage in ProductCard component

--- react-components/src/components/product-card/product-card.tsx
+++ react-components/src/components/product-card/product-card.tsx
@@ -1,9 +1,11 @@
-import { FC, HTMLAttributes } from 'react';
+import classNames from 'classnames';
+import { CSSProperties, FC, HTMLAttributes } from 'react';
+import { useImage } from '../../hooks/use-image';
 import { Product } from '../../models/product';
 import { PropsWithClassName } from '../../types/types';
 import { formatPrice } from '../../utils/functions';
-import classNames from 'classnames';
 import styles from './product-card.module.scss';
+import imageNotAvailable from '/image-not-available.jpg';
 
 export type ProductCardProps = PropsWithClassName & {
   product: Product;
@@ -15,16 +17,19 @@ export type ProductCardState = {
 
 export const ProductCard: FC<ProductCardProps> = ({ product, className, onClick }) => {
   const { title, price, thumbnail, stock } = product;
+  const loadedSrc = useImage(thumbnail, imageNotAvailable);
+  const isImageLoaded = loadedSrc === thumbnail;
+
+  const thumbnailStyle: CSSProperties = isImageLoaded
+    ? { background: `url(${loadedSrc}) 0% 0% / cover` }
+    : { backgroundImage: `url(${loadedSrc})`, backgroundSize: '100% 100%' };
 
   const isAvailable = stock > 0;
 
   return (
     <li className={classNames(styles['product-preview'], className)} onClick={onClick}>
       <div className={styles['product-item']}>
-        <div
-          className={styles['product-item__wrapper']}
-          style={{ background: `url(${thumbnail}) 0% 0% / cover` }}
-        >
+        <div className={styles['product-item__wrapper']} style={thumbnailStyle}>
           <div className={styles['product-item__text']}>
             <h4 className={styles['product-item__title']}>{title}</h4>
             <div className={styles['product-item__info']}>
