9523d6549def0acc5a562d52a02c57f7ec19f8d3
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-04-16 01:58:25 +0300

    fix: fix tests for the ProductList component

--- react-components/src/components/product-list/product-list.test.tsx
+++ react-components/src/components/product-list/product-list.test.tsx
@@ -4,45 +4,17 @@ import { Mock, describe, it, vi } from 'vitest';
 import { createFakeProducts } from '../../models/product';
 import { ProductList } from './product-list';
 
-const renderProductList = (count: number, onCardClick: Mock, renderSpinner: Mock) =>
-  render(
-    <ProductList
-      products={createFakeProducts(count)}
-      status="resolved"
-      error={null}
-      onCardClick={onCardClick}
-      renderSpinner={renderSpinner}
-    />
-  );
-
-const renderProductListWithError = (error: string, onCardClick: Mock, renderSpinner: Mock) =>
-  render(
-    <ProductList
-      products={null}
-      status="rejected"
-      error={error}
-      onCardClick={onCardClick}
-      renderSpinner={renderSpinner}
-    />
-  );
+const renderProductList = (count: number, onCardClick: Mock) =>
+  render(<ProductList products={createFakeProducts(count)} onCardClick={onCardClick} />);
 
 describe('ProductList', async () => {
   it('renders products', async () => {
     const count = 10;
-    const { unmount, container } = renderProductList(count, vi.fn(), vi.fn());
+    const { unmount, container } = renderProductList(count, vi.fn());
 
     const listItems = container.querySelectorAll(`li[class*=product-preview]`);
 
     expect(listItems).toHaveLength(count);
-
-    unmount();
-  });
-
-  it('renders error', () => {
-    const error = 'Error';
-    const { unmount, getByText } = renderProductListWithError(error, vi.fn(), vi.fn());
-
-    expect(getByText(error)).toBeInTheDocument();
 
     unmount();
   });
